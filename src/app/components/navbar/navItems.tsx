894df271309d3a69b2827cecf8a34ee1464c8248
Nestor Matias <solomatias321@gmail.com>
2021-09-27 13:47:58 +0200

    creating navbar component

---
+++ src/app/components/navbar/navItems.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+import tw from 'twin.macro';
+
+const LisContainer = styled.ul`
+    ${tw`
+        flex
+        list-none
+    `};
+`;
+
+
+export function NavItems = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
