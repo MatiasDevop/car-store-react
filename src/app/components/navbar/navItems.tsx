0fa5a64a88a676bf29bd08cf395b64d00baa8263
Nestor Matias <solomatias321@gmail.com>
2021-09-28 11:46:28 +0200

    building navitems and make it responsive

--- src/app/components/navbar/navItems.tsx
+++ src/app/components/navbar/navItems.tsx
@@ -1,19 +1,71 @@
 import React from 'react'
 import styled from 'styled-components'
 import tw from 'twin.macro';
+import { slide as Menu } from "react-burger-menu"; // fixed with typings file
+import { useMediaQuery } from "react-responsive";
+import { SCREENS } from '../responsive';
+import menuStyles from './menuStyles';
 
-const LisContainer = styled.ul`
+const ListContainer = styled.ul`
     ${tw`
         flex
         list-none
     `};
 `;
 
+const NavItem = styled.li`
+    ${tw`
+        text-sm
+        md:text-base
+        text-black
+        font-medium
+        mr-1
+        md:mr-5
+        cursor-pointer
+        transition
+        duration-300
+        hover:text-gray-700
+    `};
+`;
 
-export function NavItems = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+
+export function NavItems () {
+
+    const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+
+    if(isMobile)
+        return (
+        <Menu right styles={menuStyles}>
+            <ListContainer>
+                <NavItem>
+                    <a href="#">Home</a>
+                </NavItem>
+                <NavItem>
+                    <a href="#">Cars</a>
+                </NavItem>
+                <NavItem>
+                    <a href="#">Services</a>
+                </NavItem>
+                <NavItem>
+                    <a href="#">Contact Us</a>
+                </NavItem>
+            </ListContainer>
+        </Menu>
+        );
+
+
+    return <ListContainer>
+        <NavItem>
+            <a href="#">Home</a>
+        </NavItem>
+        <NavItem>
+            <a href="#">Cars</a>
+        </NavItem>
+        <NavItem>
+            <a href="#">Services</a>
+        </NavItem>
+        <NavItem>
+            <a href="#">Contact Us</a>
+        </NavItem>
+    </ListContainer>
 }
