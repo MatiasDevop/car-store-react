71f3912e67955407172c8239478fa78337fd3b22
Nestor Matias <solomatias321@gmail.com>
2021-09-30 11:57:07 +0200

    Book card implimented and Calendar added

--- src/app/components/bookCard/index.tsx
+++ src/app/components/bookCard/index.tsx
@@ -1,15 +1,103 @@
-import React from 'react'
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react'
 import styled from 'styled-components';
 import tw from 'twin.macro';
+import { Button } from '../button';
+import { Marginer } from '../marginer';
+
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const CardContainer = styled.div`
+    min height: 4.3em;
+    box-shadow: 0 1.3px 12px -3px rgba(0, 0, 0, 0.4);
     ${tw`
         flex
         justify-center
         items-center
-    `
-    }
+        rounded-md
+        bg-white
+        pt-1
+        pb-1
+        pr-2
+        pl-2
+        md:pt-2
+        md:pb-2
+        md:pl-9
+        md:pr-9
+    `};
 `;
 
+const ItemContainer = styled.div`
+    ${tw`flex relative`};
+`;
 
-export function BookCard() {}
+const Icon = styled.span`
+    ${tw`
+        text-red-500
+        fill-current
+        text-xs
+        md:text-base
+        mr-1
+        md:mr-3
+    `}
+`;
+
+const Name = styled.span`
+    ${tw`
+        text-gray-600
+        text-xs
+        md:text-sm
+        cursor-pointer
+    `};
+`;
+
+const LineSeparator = styled.span`
+    width: 2px;
+    height: 45%;
+    ${tw`
+        bg-gray-300
+        mr-2
+        ml-2
+        md:mr-5
+        md:ml-5
+    `}
+`;
+
+const DateCalendar = styled(Calendar)`
+    position: absolute;
+    max-width: none;
+    top: 3.5em;
+    left: -2em;
+`;
+
+export function BookCard() {
+    const [startDate, setStartDate] = useState<Date>(new Date());
+    const [isStartCalendarOpen, setStartCalendarOpen] = useState(false);
+
+    console.log("Value: ", startDate);
+
+    const toggleStartDateCalendar = () => {
+        setStartCalendarOpen(!isStartCalendarOpen);
+    }
+
+    return <CardContainer>
+        <ItemContainer>
+            <Icon>
+                <FontAwesomeIcon icon={faCalendarAlt} />
+            </Icon>
+            <Name onClick={toggleStartDateCalendar}> Pick Up Date</Name>
+            {isStartCalendarOpen && <DateCalendar value={startDate} onChange={setStartDate} />}
+        </ItemContainer>
+        <LineSeparator />
+        <ItemContainer>
+            <Icon>
+                <FontAwesomeIcon icon={faCalendarAlt} />
+            </Icon>
+            <Name>Return Date</Name>
+        </ItemContainer>
+        <Marginer direction="horizontal" margin="2em" />
+        <Button text="Book your Ride" />
+    </CardContainer>
+}
